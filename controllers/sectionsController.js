e3d91defd332e3dcbe0f106790f84822efa1ebaa
AakashTaneja <akashtaneja@gmail.com>
2024-01-30 11:49:29 +0530

    sections controller change for getting last updated

--- controllers/sectionsController.js
+++ controllers/sectionsController.js
@@ -2,13 +2,41 @@ const asyncHandler = require('express-async-handler');
 
 const { sectionsModel } = require('../models/sectionsModel');
 
+//https://api.nutshellnews.in/api/sections?section_name=section_name
+
 
 const getAllSections = asyncHandler(async (req, res) => {
 
-    console.log("Environent is prod, responding from sections database");
-    const sections = await sectionsModel.find();
-    //console.log('sections are ' + sections)
-    res.json(sections);
+    const section_name = req.query.section_name
+    const field_req = req.query.field
+
+    if (typeof (section_name) == 'undefined') {
+        console.log("Environent is prod, responding from sections database");
+        const sections = await sectionsModel.find();
+        //console.log('sections are ' + sections)
+        res.json(sections);
+    }
+    else {
+        if (typeof (field_req) == 'undefined') {
+            //console.log("Environent is prod, responding from section database");
+            filter = { db: section_name }
+            const section = await sectionsModel.find(filter);
+            res.json(section);
+        }
+        else {
+            //console.log("Environent is prod, responding field from section database");
+            filter = { db: section_name }
+            var field = field_req
+            var json = {};
+            json[field] = 1
+            console.log('field_req is ' + field_req)
+            const section = await sectionsModel.find(filter).select(json);
+            res.json(section);
+        }
+
+    }
+
+
 
 })
 
