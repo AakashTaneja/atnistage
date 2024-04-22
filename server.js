5da32c4c5d6eba3ebb90748f3c7351d9e3816e72
AakashTaneja <akashtaneja@gmail.com>
2024-04-22 09:18:04 +0530

    separate path for trending search, only using headline

--- server.js
+++ server.js
@@ -86,6 +86,51 @@ app.use('/api/search', (req, res) => {
    }
 
 });
+app.use('/api/searchtrends', (req, res) => {
+   const search_key = req.query.query
+   const full_key = req.query.full
+   var searchRegex;
+   const collectionsToSearch = ['news', 'sports', 'technology', 'entertainment', 'markets', 'health', 'science'];
+   if (typeof (full_key) == 'undefined') {
+      searchRegex = new RegExp(search_key.split(/\s+/).join('|'), 'i'); //this is to serach any of the multiple words provided.
+      //console.log('no full_key search regex is ' + searchRegex)
+   }
+   else {
+      searchRegex = new RegExp('\\b' + search_key + '\\b', 'i'); //this is to serach case insensitive.
+      //console.log('full_key search regex is ' + searchRegex)
+   }
+   //console.log('search regex is ' + searchRegex)
+   const searchCriteria = {
+      $or: [
+         { headline: { $regex: searchRegex } },
+         // { keywords: { $regex: searchRegex } },
+         // Add more fields as needed
+      ],
+   };
+
+   if (typeof (search_key) == 'undefined') {
+      console.log("Environent is prod, responding from sections database");
+      //console.log('sections are ' + sections)
+      res.json('please provide query');
+   }
+   else {
+      //console.log('key is ' + key)
+      databaseSearch(searchCriteria, collectionsToSearch)
+         .then(results => {
+            if (results) {
+               //console.log('Combined search results:', results);
+               res.json(results)
+            } else {
+               console.error('No results found.');
+               res.json('no result found')
+            }
+         })
+         .catch(error => {
+            console.error('Error:', error);
+         });
+   }
+
+});
 app.use(errorHandler);
 
 //serve static assets if in production
