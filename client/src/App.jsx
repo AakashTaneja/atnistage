cb5a045fc2b27e698d7832700ac388129f51b5af
kartik-angira <75604654+kartik-angira@users.noreply.github.com>
2025-06-12 20:44:43 +0530

    Responsive Changes
    Responsive Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import TrendingPage from "./components/TrendingPage";
@@ -12,44 +12,49 @@ import SearchPageMobile from "./components/SearchPageMobile";
 import SearchResultsMobile from "./components/SearchResultsMobile";
 import TrendingPageMobile from "./components/TrendingPageMobile";
 import SearchRecResultsMobile from "./components/SearchRecResultsMobile";
-// import AppBannerSmart from "./components/AppBannerSmart";
+import LandingPage from "./components/landingpage";
 
-//import ReactGA from 'react-ga';
+function AppContent() {
+  const location = useLocation();
+  const [searchFired, setSearchFired] = React.useState(false);
 
+  // Hide Navbar on landing page
+  const hideNavbarPaths = ["/landingpage"];
+  const shouldShowNavbar = !hideNavbarPaths.includes(location.pathname);
 
-
+  return (
+    <>
+      {shouldShowNavbar && (
+        <NavbarMaster searchFired={searchFired} setSearchFired={setSearchFired} />
+      )}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/trending" element={<TrendingPage />} />
+        <Route path="/trendingmobile" element={<TrendingPageMobile />} />
+        <Route path="/search" element={<SearchResultsDesktop />} />
+        <Route
+          path="/searchmobile"
+          element={
+            <SearchPageMobile searchFired={searchFired} setSearchFired={setSearchFired} />
+          }
+        />
+        <Route path="/searchresultmobile" element={<SearchResultsMobile />} />
+        <Route path="/recresultmobile" element={<SearchRecResultsMobile />} />
+        <Route path="/landingpage" element={<LandingPage />} />
+      </Routes>
+    </>
+  );
+}
 
 function App() {
-    // React.useEffect(()=>{
-    //     console.log("Inside useEffect");
-    //     ReactGA.initialize('G-QTDD64JWRN');
-    //     ReactGA.pageview(window.location.pathname + window.location.search);
-    // }, [])
-const [searchFired, setSearchFired] = React.useState(false)
-
-    return (
-        
-        <Router>
-            <NavbarMaster searchFired={searchFired} setSearchFired={setSearchFired}/> 
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/home" element={<Home />}></Route>
-                <Route path="/about" element={<About />}></Route>
-                <Route path="/contact" element={<Contact />}></Route>
-                <Route path="/trending" element={<TrendingPage />}></Route>
-                <Route path="/trendingmobile" element={<TrendingPageMobile />}></Route>
-                <Route path="/search" element={<SearchResultsDesktop />}></Route>
-                <Route path="/searchmobile" element={<SearchPageMobile searchFired={searchFired} setSearchFired={setSearchFired} />}></Route>
-                <Route path="/searchresultmobile" element={<SearchResultsMobile />}></Route>
-                <Route path="/recresultmobile" element={<SearchRecResultsMobile />}></Route>
-            </Routes>
-        </Router>
-       
-        
-       
-    );
-
-
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
 }
 
 export default App;
